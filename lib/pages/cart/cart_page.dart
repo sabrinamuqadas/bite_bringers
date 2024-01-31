d93bf2d53922aa015ab2823c6ddda544ef1bf5be
sabrinamuqadas <sabrinamuqadas@gmail.com>
2024-01-31 18:22:01 +0500

    Updated

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -42,12 +42,38 @@ class CartPage extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: Dimensions.height20*5,
+            top: Dimensions.height20 * 5,
             left: Dimensions.width20,
             right: Dimensions.width20,
             bottom: 0,
             child: Container(
+              margin: EdgeInsets.only(top: Dimensions.height15),
               color: Colors.red,
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (_, index) {
+                    return Container(
+                      margin: EdgeInsets.only(top: Dimensions.height10),
+                     width: double.maxFinite,
+                      height: Dimensions.height20*5,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: Dimensions.width20*5,
+                            height: Dimensions.height20*5,
+                            margin: EdgeInsets.only(bottom: Dimensions.height10),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(fit: BoxFit.cover,
+                                image: AssetImage("assets/image/icecream_2.jpg"),),
+                              borderRadius: BorderRadius.circular(Dimensions.radius20),
+                              color: Colors.white,
+
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
             ),
           ),
         ],
