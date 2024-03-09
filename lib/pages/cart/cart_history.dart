26e8f38c3130af8d50df164d330578ac157de888
sabrinamuqadas <sabrinamuqadas@gmail.com>
2024-03-10 00:57:18 +0500

    Updated on 10-03-2024
    Cart History UI PART-3

--- lib/pages/cart/cart_history.dart
+++ lib/pages/cart/cart_history.dart
@@ -7,13 +7,15 @@ import 'package:bite_bringers/widgets/big_text.dart';
 import 'package:bite_bringers/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class CartHistory extends StatelessWidget {
   const CartHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var getCartHistoryList = Get.find<CartController>().getCartHistoryList();
+    var getCartHistoryList = Get.find<CartController>()
+        .getCartHistoryList().reversed.toList();
 
     Map<String, int> cartItemsPerOrder = Map();
     for (int i = 0; i <getCartHistoryList.length; i++) {
@@ -36,10 +38,10 @@ class CartHistory extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            height: 100,
+            height: Dimensions.height10*10,
             color: AppColors.mainColor,
             width: double.maxFinite,
-            padding: EdgeInsets.only(top: 45),
+            padding: EdgeInsets.only(top: Dimensions.height45),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -60,12 +62,19 @@ class CartHistory extends StatelessWidget {
                 context: context, child: ListView(
                 children: [
                   for(int i=0; i<itemsPerOrder.length; i++) //outer loop
-                    Container(height: 120,
+                    Container(
+                      height: Dimensions.height30*4,
                       margin: EdgeInsets.only(bottom: Dimensions.height20),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          BigText(text: "09/03/2024"),
+                          ((){
+                          DateTime parseDate =  DateFormat("yyyy-MM-dd HH:mm:ss").parse(getCartHistoryList[listCounter].time!);
+                          var inputDate = DateTime.parse(parseDate.toString());
+                            var outputFormat = DateFormat("dd/MM/yyyy hh:mm a");
+                            var outputDate = outputFormat.format(inputDate);
+                            return BigText(text: outputDate);
+                          }()),
                           SizedBox(height: Dimensions.height10,),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -78,8 +87,8 @@ class CartHistory extends StatelessWidget {
                                     listCounter++;
                                   }
                                   return index<=2?Container(
-                                    height: 80,
-                                    width: 80,
+                                    height: Dimensions.height20*4 ,
+                                    width: Dimensions.width20*4,
                                     margin: EdgeInsets.only(right: Dimensions.width10/2),
                                     decoration: BoxDecoration(
                                       borderRadius: BorderRadius.circular(Dimensions.radius15/2),
@@ -92,7 +101,7 @@ class CartHistory extends StatelessWidget {
                                 }),
                               ),
                               Container(
-                                height: 80,
+                                height: Dimensions.height20*4,
                                 child: Column(
                                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                   crossAxisAlignment: CrossAxisAlignment.end,
